926b662f79bcd2e622e3376f4d3ce7e1f68aeab5
Yeji Han <hany7@hawaii.edu>
2020-11-06 01:18:05 -1000

    Done

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -12,12 +12,12 @@ class ContactAdmin extends React.Component {
             <Image
                 floated='right'
                 size='mini'
-                src={this.props.ContactAdmin.image}
+                src={this.props.contact.image}
             />
-            <Card.Header>{this.props.ContactAdmin.firstName} {this.props.ContactAdmin.lastName}</Card.Header>
-            <Card.Meta>{this.props.ContactAdmin.address}</Card.Meta>
+            <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+            <Card.Meta>{this.props.contact.address}</Card.Meta>
             <Card.Description>
-              {this.props.ContactAdmin.description}
+              {this.props.contact.description}
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
@@ -30,7 +30,7 @@ class ContactAdmin extends React.Component {
 
 /** Require a document to be passed to this component. */
 ContactAdmin.propTypes = {
-  ContactAdmin: PropTypes.object.isRequired,
+  contact: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
